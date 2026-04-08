6dc190c3699925e6a773bea6662a10b888389695
Luka Forder <lukaforder@gmail.com>
2026-04-08 21:40:50 +0100

    feat: redesigned registry

--- src/routes/(app)/server/registry/+page.svelte
+++ src/routes/(app)/server/registry/+page.svelte
@@ -6,8 +6,10 @@
 	import * as InputGroup from '@coral-os/component-library/ui/input-group/index.js';
 
 	import { Input } from '@coral-os/component-library/ui/input/index.js';
+	import { Button } from '@coral-os/component-library/ui/button/index.js';
 	import { Separator } from '@coral-os/component-library/ui/separator/index.js';
 	import * as Dialog from '@coral-os/component-library/ui/dialog/index.js';
+	import * as Avatar from '@coral-os/component-library/ui/avatar/index.js';
 
 	import IconCrane from 'phosphor-icons-svelte/IconCraneRegular.svelte';
 	import { appContext } from '$lib/context';
@@ -50,8 +52,8 @@
 		</Breadcrumb.List>
 	</Breadcrumb.Root>
 </header>
-<main class="m-auto h-full max-w-3xl p-4">
-	<header class="mb-2">
+<main class="flex min-h-0 grow flex-col overflow-hidden p-4">
+	<header class="mb-2 w-1/2">
 		<InputGroup.Root>
 			<InputGroup.Input placeholder="Search..." bind:value={search} />
 			<InputGroup.Addon>
@@ -65,53 +67,79 @@
 			</InputGroup.Addon>
 		</InputGroup.Root>
 	</header>
-	{#each filtered as catalog}
-		<Card.Root>
-			<Card.Header>
-				<Card.Title
-					>{catalog.identifier.type.charAt(0).toLocaleUpperCase() +
-						catalog.identifier.type.slice(1)} Agents</Card.Title
-				>
-			</Card.Header>
-			<Card.Content class="flex  flex-col gap-2">
-				{#if catalog.agents.length == 0}
-					<span class="text-muted-foreground">No agents found.</span>
+	<ol class="flex flex-col gap-4 overflow-y-scroll">
+		{#each filtered as catalog}
+			<li>
+				{#if catalog.agents.length !== 0}
+					<h1 class="w-full py-4 text-left text-2xl">
+						{catalog.identifier.type.charAt(0).toLocaleUpperCase() +
+							catalog.identifier.type.slice(1)}
+						Agents
+					</h1>
 				{/if}
-				{#each catalog.agents as agent}
-					<Dialog.Root>
-						<Dialog.Trigger class="text-left"
-							><Item.Root variant="outline" class="hover:bg-sidebar p-2 px-2.5">
-								<Item.Content>
-									<Item.Title
-										>{agent.name}{#each agent.versions as version}<Badge variant="outline"
-												>{version}</Badge
-											>{/each}
-									</Item.Title>
-									<Item.Description class="line-clamp-1 truncate">
-										{#await ctx.server.lookupAgent( { name: agent.name, version: agent.versions[0]!, registrySourceId: catalog.identifier } )}
-											<Skeleton class="h-4 w-full" />
-										{:then details}
-											{details.registryAgent.info.description}
-										{/await}
-									</Item.Description>
-								</Item.Content>
-							</Item.Root></Dialog.Trigger
-						>
-						<Dialog.Content>
-							<Dialog.Header>
-								<Dialog.Title>{agent.name}</Dialog.Title>
-								<Dialog.Description class="min-h-52">
-									{#await ctx.server.lookupAgent( { name: agent.name, version: agent.versions[0]!, registrySourceId: catalog.identifier } )}
-										<Skeleton class="h-4 w-full" />
-									{:then details}
-										{details.registryAgent.info.description}
-									{/await}
-								</Dialog.Description>
-							</Dialog.Header>
-						</Dialog.Content>
-					</Dialog.Root>
-				{/each}
-			</Card.Content>
-		</Card.Root>
-	{/each}
+				<ol class="grid grid-cols-[repeat(auto-fit,minmax(320px,0fr))] gap-4">
+					{#each catalog.agents as agent}
+						<li class="h-[250px] w-xs grow rounded-md">
+							<Dialog.Root>
+								<Dialog.Trigger class="h-[250px] w-full text-left">
+									<Card.Root class="h-full grow">
+										<Card.Header class="flex gap-2">
+											<Avatar.Root class="size-12">
+												<Avatar.Image
+													class="bg-cover object-cover"
+													src={agent.icon_url}
+													alt={agent.name.charAt(0).toUpperCase()}
+												/>
+												<Avatar.Fallback>{agent.name.charAt(0).toUpperCase()}</Avatar.Fallback>
+											</Avatar.Root>
+											<div class="flex flex-col gap-1">
+												<Card.Title class="font-bold">{agent.name}</Card.Title>
+												<Card.Description>By {agent.name ?? 'Unknown developer'}</Card.Description>
+											</div>
+										</Card.Header>
+										<Card.Content class="flex w-full grow flex-col gap-2">
+											{#await ctx.server.lookupAgent( { name: agent.name, version: agent.versions[0]!, registrySourceId: catalog.identifier } )}
+												<Skeleton class="h-4 w-full" />
+											{:then details}
+												<p class="line-clamp-4 overflow-ellipsis">
+													{details.registryAgent.info.description}
+												</p>
+											{/await}
+											<!-- {#if agent.tags.length > 0}
+													<div class="flex flex-wrap gap-1">
+														{#each agent.tags.slice(0, 3) as tag}
+															<span class="bg-muted text-muted-foreground rounded-full px-2 py-0.5 text-xs">
+																{tag}
+															</span>
+														{/each}
+													</div>
+												{/if} -->
+										</Card.Content>
+									</Card.Root>
+								</Dialog.Trigger>
+								<Dialog.Content>
+									<Dialog.Header>
+										<Dialog.Title>{agent.name}</Dialog.Title>
+										<Dialog.Description class="min-h-52">
+											{#await ctx.server.lookupAgent( { name: agent.name, version: agent.versions[0]!, registrySourceId: catalog.identifier } )}
+												<Skeleton class="h-4 w-full" />
+											{:then details}
+												{details.registryAgent.info.description}
+											{/await}
+										</Dialog.Description>
+									</Dialog.Header>
+								</Dialog.Content>
+							</Dialog.Root>
+							<Button
+								href="/{agent.developer ?? 'coral_protocol'}/{encodeURI(agent.slug)}"
+								variant="ghost"
+								size="sm"
+								class="h-fit w-full rounded-md p-0 drop-shadow-[#ff5c0026]! transition hover:drop-shadow-lg"
+							></Button>
+						</li>
+					{/each}
+				</ol>
+			</li>
+		{/each}
+	</ol>
 </main>
