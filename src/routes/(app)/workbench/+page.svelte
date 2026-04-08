4ff08236387d359307677181f24b8e3e84c32a38
Luka Forder <lukaforder@gmail.com>
2026-04-08 17:02:28 +0100

    Feat/tour (#132)
    * feat: redesigned tour dom component selection
    
    * feat: new tour and setup
    
    * fix: remove extra thing

--- src/routes/(app)/workbench/+page.svelte
+++ src/routes/(app)/workbench/+page.svelte
@@ -76,6 +76,7 @@
 	import AgentPane from './panes/AgentPane.svelte';
 	import TemplateSaver from './TemplateSaver.svelte';
 	import { getSessionDataFromTemplateName } from './templates/TemplateLib';
+	import { tourTarget } from '$lib/components/tour/tourTarget';
 
 	function sourceToRegistryId(source: AgentSource): RegistryAgentIdentifier['registrySourceId'] {
 		switch (source) {
@@ -490,7 +491,7 @@
 							minSize={25}
 							defaultSize={50}
 						>
-							<Menubar.Root class="bg-sidebar border-0 border-b">
+							<Menubar.Root class="bg-sidebar w-full border-0 border-b">
 								<Menubar.Menu>
 									<Menubar.Trigger class="gap-1">Session<IconCaretDown /></Menubar.Trigger>
 									<Menubar.Content>
@@ -545,13 +546,12 @@
 									</Menubar.Content>
 								</Menubar.Menu>
 								<Menubar.Menu>
-									<Menubar.Trigger class="relative ml-auto gap-2">
-										<IconPlusCircle class="size-5" />
-										Add agents
-										{#if $formData.agents.length < 1}
-											<Pip size={2} color="accent" />
-										{/if}
-									</Menubar.Trigger>
+									<div use:tourTarget={'add-agents'} class="ml-auto">
+										<Menubar.Trigger class="relative  gap-2">
+											<IconPlusCircle class="size-5" />
+											Add agents
+										</Menubar.Trigger>
+									</div>
 									<Menubar.Content>
 										<AgentPicker
 											server={ctx.server}
