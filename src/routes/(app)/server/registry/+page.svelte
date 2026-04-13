7c69fccb752cf264308995f71f8e110856d062e1
Luka Forder <lukaforder@gmail.com>
2026-04-13 19:30:09 +0100

    feat: registry overhaul (#133)
    * feat: redesigned registry
    
    * feat: new design for cards
    
    * feat: better skeleton for loading

--- src/routes/(app)/server/registry/+page.svelte
+++ src/routes/(app)/server/registry/+page.svelte
@@ -6,8 +6,10 @@
 	import * as InputGroup from '@coral-os/component-library/ui/input-group/index.js';
 
 	import { Input } from '@coral-os/component-library/ui/input/index.js';
+	import { Button } from '@coral-os/component-library/ui/button/index.js';
 	import { Separator } from '@coral-os/component-library/ui/separator/index.js';
 	import * as Dialog from '@coral-os/component-library/ui/dialog/index.js';
+	import * as Avatar from '@coral-os/component-library/ui/avatar/index.js';
 
 	import IconCrane from 'phosphor-icons-svelte/IconCraneRegular.svelte';
 	import { appContext } from '$lib/context';
@@ -50,8 +52,8 @@
 		</Breadcrumb.List>
 	</Breadcrumb.Root>
 </header>
-<main class="m-auto h-full max-w-3xl p-4">
-	<header class="mb-2">
+<main class="flex min-h-0 grow flex-col overflow-hidden p-4">
+	<header class="mb-2 md:w-[400px]">
 		<InputGroup.Root>
 			<InputGroup.Input placeholder="Search..." bind:value={search} />
 			<InputGroup.Addon>
@@ -65,53 +67,79 @@
 			</InputGroup.Addon>
 		</InputGroup.Root>
 	</header>
-	{#each filtered as catalog}
-		<Card.Root>
-			<Card.Header>
-				<Card.Title
-					>{catalog.identifier.type.charAt(0).toLocaleUpperCase() +
-						catalog.identifier.type.slice(1)} Agents</Card.Title
-				>
-			</Card.Header>
-			<Card.Content class="flex  flex-col gap-2">
-				{#if catalog.agents.length == 0}
-					<span class="text-muted-foreground">No agents found.</span>
+	<ol class="flex flex-col gap-4 overflow-y-scroll">
+		{#each filtered as catalog}
+			<li>
+				{#if catalog.agents.length !== 0}
+					<h1 class="w-full py-4 text-left text-2xl">
+						{catalog.identifier.type.charAt(0).toLocaleUpperCase() +
+							catalog.identifier.type.slice(1)}
+						Agents
+					</h1>
 				{/if}
-				{#each catalog.agents as agent}
-					<Dialog.Root>
-						<Dialog.Trigger class="text-left"
-							><Item.Root variant="outline" class="hover:bg-sidebar p-2 px-2.5">
-								<Item.Content>
-									<Item.Title
-										>{agent.name}{#each agent.versions as version}<Badge variant="outline"
-												>{version}</Badge
-											>{/each}
-									</Item.Title>
-									<Item.Description class="line-clamp-1 truncate">
-										{#await ctx.server.lookupAgent( { name: agent.name, version: agent.versions[0]!, registrySourceId: catalog.identifier } )}
-											<Skeleton class="h-4 w-full" />
-										{:then details}
-											{details.registryAgent.info.description}
-										{/await}
-									</Item.Description>
-								</Item.Content>
-							</Item.Root></Dialog.Trigger
-						>
-						<Dialog.Content>
-							<Dialog.Header>
-								<Dialog.Title>{agent.name}</Dialog.Title>
-								<Dialog.Description class="min-h-52">
-									{#await ctx.server.lookupAgent( { name: agent.name, version: agent.versions[0]!, registrySourceId: catalog.identifier } )}
-										<Skeleton class="h-4 w-full" />
-									{:then details}
-										{details.registryAgent.info.description}
-									{/await}
-								</Dialog.Description>
-							</Dialog.Header>
-						</Dialog.Content>
-					</Dialog.Root>
-				{/each}
-			</Card.Content>
-		</Card.Root>
-	{/each}
+				<ol class="grid grid-cols-[repeat(auto-fit,minmax(320px,0fr))] gap-4">
+					{#each catalog.agents as agent}
+						{#await ctx.server.lookupAgent( { name: agent.name, version: agent.versions[0]!, registrySourceId: catalog.identifier } )}
+							<div class="bg-foreground/5 h-[250px] w-xs border"></div>
+						{:then details}
+							<li class="h-[250px] w-xs grow rounded-md">
+								<Dialog.Root>
+									<Dialog.Trigger class=" h-[250px] w-full text-left">
+										<Card.Root class="hover:dark:bg-ring/20 hover:bg-ring/10 h-full grow">
+											<Card.Header class="flex gap-2">
+												<Avatar.Root class="size-12">
+													<Avatar.Image
+														class="bg-cover object-cover"
+														src={details.extension?.iconUrl}
+														alt={agent.name.charAt(0).toUpperCase()}
+													/>
+													<Avatar.Fallback>{agent.name.charAt(0).toUpperCase()}</Avatar.Fallback>
+												</Avatar.Root>
+												<div class="flex flex-col gap-1">
+													<Card.Title class="font-bold">{agent.name}</Card.Title>
+													<Card.Description>
+														{details.extension?.developer
+															? 'By ' + details.extension.developer
+															: 'Unknown developer'}</Card.Description
+													>
+												</div>
+											</Card.Header>
+											<Card.Content class="flex w-full grow flex-col gap-2">
+												<p class="line-clamp-4 overflow-ellipsis">
+													{details.registryAgent.info.description}
+												</p>
+												{#if details.registryAgent?.marketplace?.keywords && details.registryAgent.marketplace.keywords.length > 0}
+													<div class="flex flex-wrap gap-1">
+														{#each details.registryAgent.marketplace.keywords.slice(0, 3) as keyword}
+															<span
+																class="bg-muted text-muted-foreground rounded-full px-2 py-0.5 text-xs"
+															>
+																{keyword}
+															</span>
+														{/each}
+													</div>
+												{/if}
+											</Card.Content>
+										</Card.Root>
+									</Dialog.Trigger>
+									<Dialog.Content>
+										<Dialog.Header>
+											<Dialog.Title>{agent.name}</Dialog.Title>
+											<Dialog.Description class="min-h-52">
+												{#await ctx.server.lookupAgent( { name: agent.name, version: agent.versions[0]!, registrySourceId: catalog.identifier } )}
+													<Skeleton class="h-4 w-full" />
+												{:then details}
+													{details.registryAgent.info.description}
+												{/await}
+											</Dialog.Description>
+										</Dialog.Header>
+									</Dialog.Content>
+								</Dialog.Root>
+							</li>
+						{/await}
+					{/each}
+				</ol>
+			</li>
+		{/each}
+	</ol>
 </main>
