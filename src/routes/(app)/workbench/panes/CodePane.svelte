d10780cfe64631da4d6ff1485192dddea17d5e16
Luka Forder <lukaforder@gmail.com>
2026-04-23 10:30:55 +0100

    feat: new themes for code mirror, plus dark/light mode support

--- src/routes/(app)/workbench/panes/CodePane.svelte
+++ src/routes/(app)/workbench/panes/CodePane.svelte
@@ -6,7 +6,10 @@
 
 	import CodeMirror from 'svelte-codemirror-editor';
 	import { json } from '@codemirror/lang-json';
-	import { dracula } from '@uiw/codemirror-theme-dracula';
+	import { atomone } from '@uiw/codemirror-theme-atomone';
+	import { quietlight } from '@uiw/codemirror-theme-quietlight';
+
+	import { mode } from 'mode-watcher';
 
 	import { ScrollArea } from '@coral-os/component-library/ui/scroll-area/index.js';
 	import { Separator } from '@coral-os/component-library/ui/separator/index.js';
@@ -24,17 +27,27 @@
 	const editorTab = new PersistedState('sessionEditorTab', 'json');
 	let payloadJson = $derived(ctx.payload ? JSON.stringify(ctx.payload, null, 4) : '');
 	let jsonDirty = $state(false);
+
+	let theme = $derived.by(() => {
+		switch (mode.current) {
+			case 'light':
+				return quietlight;
+			case 'dark':
+				return atomone;
+			default:
+				return atomone;
+		}
+	});
 </script>
 
 <Tabs.Root bind:value={editorTab.current} class="grow gap-0 overflow-hidden">
 	<Tabs.List class="bg-sidebar  flex w-full justify-start rounded-none border-b *:rounded-none">
 		<Tabs.Trigger value="json" class="grow-0">Session editor{jsonDirty ? '*' : ''}</Tabs.Trigger>
-		<Separator orientation="vertical" />
-		<Tabs.Trigger value="js" class="grow-0">JavaScript</Tabs.Trigger>
+		<!-- <Tabs.Trigger value="js" class="grow-0">JavaScript</Tabs.Trigger>
 		<Tabs.Trigger value="py" class="grow-0">Python</Tabs.Trigger>
-		<!-- <Tabs.Trigger value="curl" class="grow-0">cURL</Tabs.Trigger> -->
+		<Tabs.Trigger value="curl" class="grow-0">cURL</Tabs.Trigger> -->
 	</Tabs.List>
-	<Tabs.Content value="json" class="relative overflow-hidden">
+	<Tabs.Content value="json" class="relative h-full overflow-hidden">
 		<section class="absolute top-5 right-5 z-10 flex flex-col gap-2">
 			<CopyButton value={payloadJson} />
 			{#if jsonDirty}
@@ -55,21 +68,19 @@
 				</span>
 			{/if}
 		</section>
-		<ScrollArea class="size-full">
-			<CodeMirror
-				bind:value={payloadJson}
-				onchange={() => {
-					jsonDirty = true;
-				}}
-				lang={json()}
-				tabSize={4}
-				theme={dracula}
-				lineWrapping={true}
-				class="size-full [&_.cm-content]:p-0! [&>*]:size-full "
-			/>
-		</ScrollArea>
+		<CodeMirror
+			bind:value={payloadJson}
+			onchange={() => {
+				jsonDirty = true;
+			}}
+			lang={json()}
+			tabSize={4}
+			{theme}
+			lineWrapping={true}
+			class="size-full [&_.cm-content]:p-0! [&>*]:size-full "
+		/>
 	</Tabs.Content>
-	<Tabs.Content value="js" class="relative overflow-y-auto">
+	<!-- <Tabs.Content value="js" class="relative overflow-y-auto">
 		{#if ctx.payload}
 			<CodeSnippet snippet="javascript" body={ctx.payload} />
 		{/if}
@@ -78,5 +89,5 @@
 		{#if ctx.payload}
 			<CodeSnippet snippet="python" body={ctx.payload} />
 		{/if}
-	</Tabs.Content>
+	</Tabs.Content> -->
 </Tabs.Root>
