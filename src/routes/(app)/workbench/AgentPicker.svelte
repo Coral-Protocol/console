578854c1f71a985b2db749047c8187ae1a004510
Luka Forder <lukaforder@gmail.com>
2026-05-01 00:57:21 +0100

    feat: better hovercard

--- src/routes/(app)/workbench/AgentPicker.svelte
+++ src/routes/(app)/workbench/AgentPicker.svelte
@@ -3,6 +3,8 @@
 	import * as HoverCard from '@coral-os/component-library/ui/hover-card/index.js';
 	import type { CoralServer } from '$lib/CoralServer.svelte';
 	import type { components } from '$generated/api';
+	import * as Avatar from '@coral-os/component-library/ui/avatar/index.js';
+	import * as Card from '@coral-os/component-library/ui/card/index.js';
 
 	let {
 		server,
@@ -49,13 +51,46 @@
 
 						<HoverCard.Content
 							side="right"
-							class="max-w-1/2 min-w-full whitespace-pre-wrap"
+							class=" min-w-full whitespace-pre-wrap"
 							hideWhenDetached
 						>
 							{#await server.lookupAgent( { name: agent.name, version: agent.versions[0]!, registrySourceId: catalog.identifier } )}
 								<span>Loading details...</span>
 							{:then details}
-								{details.registryAgent.info.description}
+								<div class="flex gap-2">
+									<Avatar.Root class="size-12">
+										<Avatar.Image
+											class="bg-cover object-cover"
+											src={details.extension?.iconUrl}
+											alt={agent.name.charAt(0).toUpperCase()}
+										/>
+										<Avatar.Fallback>{agent.name.charAt(0).toUpperCase()}</Avatar.Fallback>
+									</Avatar.Root>
+									<div class="flex flex-col gap-1">
+										<Card.Title class="font-bold">{agent.name}</Card.Title>
+										<Card.Description>
+											{details.extension?.developer
+												? 'By ' + details.extension.developer
+												: 'Unknown developer'}</Card.Description
+										>
+									</div>
+								</div>
+								<div class="flex w-full grow flex-col gap-2">
+									<p class="line-clamp-4 overflow-ellipsis">
+										{details.registryAgent.info.description}
+									</p>
+									{#if details.registryAgent?.marketplace?.keywords && details.registryAgent.marketplace.keywords.length > 0}
+										<div class="flex flex-wrap gap-1">
+											{#each details.registryAgent.marketplace.keywords.slice(0, 3) as keyword}
+												<span
+													class="bg-muted text-muted-foreground rounded-full px-2 py-0.5 text-xs"
+												>
+													{keyword}
+												</span>
+											{/each}
+										</div>
+									{/if}
+								</div>
 							{/await}
 						</HoverCard.Content>
 					</HoverCard.Root>
