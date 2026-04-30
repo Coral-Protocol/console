49046951c7f5457a686eb21e75996ec8a745a6bd
Luka Forder <lukaforder@gmail.com>
2026-05-01 00:47:50 +0100

    fix: fixed quirks with add agents drop down including scrolling away the hovercard and same name items

--- src/routes/(app)/workbench/AgentPicker.svelte
+++ src/routes/(app)/workbench/AgentPicker.svelte
@@ -14,18 +14,32 @@
 			catalogId: components['schemas']['AgentRegistrySource']['identifier']
 		) => void;
 	} = $props();
+
+	let openId: string | null = $state(null);
+
+	function handleScroll() {
+		openId = null;
+	}
 </script>
 
-<HoverCard.Root closeDelay={0}>
-	<Command.Root>
-		<Command.Input placeholder="Search agents..." />
-		<Command.List>
-			<Command.Empty>No agents found.</Command.Empty>
-			{#each Object.values(server.catalogs) as catalog}
-				<Command.Group heading={catalog.identifier.type}>
-					{#each Object.values(catalog.agents) as agent}
+<Command.Root>
+	<Command.Input placeholder="Search agents..." />
+	<Command.List onscroll={handleScroll}>
+		<Command.Empty>No agents found.</Command.Empty>
+		{#each Object.values(server.catalogs) as catalog}
+			<Command.Group heading={catalog.identifier.type}>
+				{#each Object.values(catalog.agents) as agent}
+					{@const id = `${catalog.identifier.type}-${agent.name}`}
+					<HoverCard.Root
+						closeDelay={0}
+						open={openId === id}
+						onOpenChange={(v) => {
+							openId = v ? id : null;
+						}}
+					>
 						<HoverCard.Trigger class="m-0">
 							<Command.Item
+								value={`${catalog.identifier.type}:${agent.name}:${agent.versions[0]}`}
 								class="w-full cursor-pointer border-b px-4 py-2"
 								onSelect={() => onSelect?.(agent, catalog.identifier)}
 							>
@@ -33,16 +47,20 @@
 							</Command.Item>
 						</HoverCard.Trigger>
 
-						<HoverCard.Content side="right" class="max-w-1/2 min-w-full whitespace-pre-wrap">
+						<HoverCard.Content
+							side="right"
+							class="max-w-1/2 min-w-full whitespace-pre-wrap"
+							hideWhenDetached
+						>
 							{#await server.lookupAgent( { name: agent.name, version: agent.versions[0]!, registrySourceId: catalog.identifier } )}
 								<span>Loading details...</span>
 							{:then details}
 								{details.registryAgent.info.description}
 							{/await}
 						</HoverCard.Content>
-					{/each}
-				</Command.Group>
-			{/each}
-		</Command.List>
-	</Command.Root>
-</HoverCard.Root>
+					</HoverCard.Root>
+				{/each}
+			</Command.Group>
+		{/each}
+	</Command.List>
+</Command.Root>
