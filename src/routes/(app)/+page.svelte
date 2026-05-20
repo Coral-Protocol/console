ca40e25388295ee27195dd309a17246af506a4f6
Luka Forder <lukaforder@gmail.com>
2026-05-20 11:34:57 +0100

    fix: home page text

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -6,60 +6,9 @@
 	import { goto } from '$app/navigation';
 	import { base } from '$app/paths';
 	import Header from '$lib/components/header.svelte';
+	import * as UnderlineTabs from '@coral-os/component-library/ui/underline-tabs/index.js';
 </script>
 
 <Header />
-<!-- 
-<section class="m-auto">
-	<h1>Welcome back</h1>
-	<div>
-		<p>Here's what's happening with your sessions today:</p>
-		<ul>
-			<li>You have 3 active sessions.</li>
-			<li>Your most recent session was created 2 hours ago.</li>
-			<li>You have 5 sessions that have completed their tasks.</li>
-		</ul>
-	</div>
-	<div class="grid grid-cols-2 gap-4">
-		<button onclick={() => goto(`${base}/workbench`)}>
-			<Card.Root>
-				<Card.Content>
-					<p>Card Content</p>
-				</Card.Content>
-				<Card.Footer>
-					<p>Workbench</p>
-				</Card.Footer>
-			</Card.Root>
-		</button>
-		<button onclick={() => goto(`${base}/workbench`)}>
-			<Card.Root>
-				<Card.Content>
-					<p>Card Content</p>
-				</Card.Content>
-				<Card.Footer>
-					<p>Workbench</p>
-				</Card.Footer>
-			</Card.Root>
-		</button>
-		<button onclick={() => goto(`${base}/workbench`)}>
-			<Card.Root>
-				<Card.Content>
-					<p>Card Content</p>
-				</Card.Content>
-				<Card.Footer>
-					<p>Workbench</p>
-				</Card.Footer>
-			</Card.Root>
-		</button>
-		<button onclick={() => goto(`${base}/workbench`)}>
-			<Card.Root>
-				<Card.Content>
-					<p>Card Content</p>
-				</Card.Content>
-				<Card.Footer>
-					<p>Workbench</p>
-				</Card.Footer>
-			</Card.Root>
-		</button>
-	</div>
-</section> -->
+
+<h1 class="text-muted-foreground m-auto">Welcome back</h1>
