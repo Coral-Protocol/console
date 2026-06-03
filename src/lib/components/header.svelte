91da20f3115822a1f95f80ad36f7e8f27bc05c01
Luka Forder <lukaforder@gmail.com>
2026-06-03 20:15:24 +0100

    fix: breadcrumb override feature + an override for home

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,8 +1,10 @@
-<script>
+<script lang="ts">
 	import * as Card from '@coral-os/component-library/ui/card/index.js';
 	import { Separator } from '@coral-os/component-library/ui/separator/index.js';
 	import * as Sidebar from '@coral-os/component-library/ui/sidebar/index.js';
 	import Breadcrumbs from '$lib/components/Breadcrumbs.svelte';
+
+	let { override }: { override?: string } = $props();
 </script>
 
 <header class="bg-background sticky top-0 flex h-16 shrink-0 items-center gap-2 p-2">
@@ -10,7 +12,7 @@
 		<Card.Content class="relative flex h-full items-center gap-2 px-3">
 			<Sidebar.Trigger class="-ml-1" />
 			<Separator orientation="vertical" class="mr-2 h-4" />
-			<Breadcrumbs />
+			<Breadcrumbs {override} />
 		</Card.Content>
 	</Card.Root>
 </header>
