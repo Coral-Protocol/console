178a9398a297d6ccc42285d7ebd50b53054f3c4e
Luka Forder <lukaforder@gmail.com>
2026-06-10 13:22:26 +0100

    wip: overview page

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -16,6 +16,9 @@
 	import { formatDistanceToNow, format } from 'date-fns';
 	import { Badge } from '@coral-os/component-library/components/ui/badge/index.js';
 
+	import { activity } from '$lib/session.svelte';
+	import { fade } from 'svelte/transition';
+
 	let ctx = appContext.get();
 
 	let namespaces = $derived(ctx.server.namespaces);
@@ -24,28 +27,21 @@
 	let activeNamespace: string = $state('default');
 </script>
 
-<Header override="Home" />
+<Header override="Overview" />
 
-<main class="flex h-full w-full flex-col gap-6 p-6">
+<main class="flex h-full max-h-2/3 min-h-0 w-full grow flex-col gap-6 overflow-hidden p-6">
 	<section class="flex items-center justify-between">
 		<div>
 			<h1 class="text-2xl font-semibold">Welcome back</h1>
 			<p class="text-muted-foreground text-sm">Here's what's happening within your Coral server.</p>
 		</div>
-
-		<!-- <div class="flex gap-2">
-			<Button class="btn">Connect Server</Button>
-			<Button class="btn btn-primary">Create Session</Button>
-			<Button class="btn">Register Agent</Button>
-			<Button class="btn">Start Thread</Button>
-		</div> -->
 	</section>
 
 	<section class="grid grid-cols-5 gap-4">
 		<Card.Root class="flex h-full flex-col justify-between gap-1 p-6">
 			<p class="text-sm">Namespaces</p>
 			<p class="text-2xl font-semibold">{namespaces.length}</p>
-			<p class="text-muted-foreground text-xs">excluding default</p>
+			<p class="text-muted-foreground text-xs opacity-0">default</p>
 		</Card.Root>
 
 		<Card.Root class="flex h-full flex-col justify-between gap-1 p-6">
@@ -61,48 +57,30 @@
 		<Card.Root class="flex h-full flex-col justify-between gap-1 p-6">
 			<p class="text-sm">Threads</p>
 			<p class="text-2xl font-semibold">{sessions.length}</p>
-			<p class="text-muted-foreground text-xs">across all namespaces</p>
+			<p class="text-muted-foreground text-xs opacity-0">across all namespaces</p>
 		</Card.Root>
-
-		<Card.Root class="flex h-full flex-col justify-between gap-1 p-6">
-			<p class="text-sm">Messages / min</p>
-			<p class="text-2xl font-semibold">{sessions.length}</p>
-			<p class="text-muted-foreground text-xs">over last x hours</p>
-		</Card.Root>
-
 		<!-- <Card.Root class="flex h-full flex-col justify-between gap-1 p-6">
 			<p class="text-sm">Uptime</p>
 			<p class="text-2xl font-semibold">{sessions.length}</p>
 			<p class="text-muted-foreground text-xs">trending</p>
 		</Card.Root> -->
 	</section>
-
-	<section class="grid grow grid-cols-2 gap-4">
-		<div class="col-span-1">
-			<Card.Root class="h-full py-0.5">
-				<UnderlineTabs.Root bind:value={activeNamespace} class="gap-0">
-					<UnderlineTabs.List>
-						<span
-							class="text-muted-foreground mr-2 cursor-default border-r px-2 text-sm font-medium"
-							>Namespaces</span
-						>
-
-						{#each ctx.server.namespaces as namespace}
-							<UnderlineTabs.Trigger value={namespace}
-								>{namespace}
-								<Badge variant="secondary"
-									>{sessions.filter((s) => s.namespace === namespace).length}</Badge
-								></UnderlineTabs.Trigger
+	{#if false}
+		<section class="grid min-h-0 grow grid-cols-2 gap-4">
+			<section>
+				<h2 class="py-2 text-xl">Recent sessions</h2>
+				<Card.Root class="h-full min-h-0 gap-0 overflow-hidden p-0 ">
+					<!-- <UnderlineTabs.Root bind:value={activeNamespace} class="gap-0">
+						<UnderlineTabs.List class="overflow-x-auto overflow-y-hidden">
+							<span class="text-muted-foreground mr-2 cursor-default border-r px-2 text-sm font-medium"
+								>Namespaces</span
 							>
-						{:else}
-							<UnderlineTabs.Trigger value="default"
-								>Default
-								<Badge variant="secondary">0</Badge></UnderlineTabs.Trigger
-							>
-						{/each}
-					</UnderlineTabs.List>
-
-					<Card.Content class="p-0">
+							{#each ctx.server.namespaces as namespace}
+								<UnderlineTabs.Trigger value={namespace}>{namespace}</UnderlineTabs.Trigger>
+							{/each}
+						</UnderlineTabs.List>
+					</UnderlineTabs.Root> -->
+					<Card.Content class="min-h-0 overflow-y-auto p-0">
 						<Table.Root>
 							<Table.Header>
 								<Table.Row class="*:text-muted-foreground text-sm *:font-normal">
@@ -145,7 +123,7 @@
 										<Table.Cell class="text-end">$0.00</Table.Cell>
 									</Table.Row>
 								{:else}
-									<Table.Row>
+									<Table.Row class="fade-in">
 										<Table.Cell colspan={4} class="text-center text-sm text-muted-foreground">
 											No active sessions in this namespace.
 										</Table.Cell>
@@ -154,24 +132,65 @@
 							</Table.Body>
 						</Table.Root>
 					</Card.Content>
-				</UnderlineTabs.Root>
-			</Card.Root>
-		</div>
+				</Card.Root>
+			</section>
 
-		<Card.Root>
-			<Card.Header>
-				<Card.Title>Live Activity</Card.Title>
-			</Card.Header>
-			<Card.Content class="space-y-3 text-sm">
-				<p>Agent Writer registered</p>
-				<p>Researcher sent message</p>
-				<p>Thread Data Pipeline started</p>
-				<p>Summarizer went offline</p>
-			</Card.Content>
-		</Card.Root>
-	</section>
+			<section>
+				<h2 class="py-2 text-xl">Recent events</h2>
 
-	<section class="grid grid-cols-2 gap-4">
+				<Card.Root class="h-full min-h-0 overflow-hidden py-0">
+					<Card.Content class="min-h-0  overflow-y-auto p-0">
+						<!-- <span
+							class="text-muted-foreground mr-2 block h-9 cursor-default content-center border-b px-2 text-sm font-medium"
+							>Live events (last 30)</span
+						> -->
+						<Table.Root>
+							<Table.Header>
+								<Table.Row class="*:text-muted-foreground text-sm *:font-normal">
+									<Table.Head class="w-[100px]">Type</Table.Head>
+									<Table.Head class="w-[100px]">Namespace</Table.Head>
+									<Table.Head class="text-end">Timestamp</Table.Head>
+								</Table.Row>
+							</Table.Header>
+							<Table.Body>
+								{#each activity.current.events as event}
+									<Table.Row class="odd:bg-muted/30">
+										<Table.Cell>
+											<Tooltip.Root>
+												{event.type.replaceAll('_', ' ')}
+											</Tooltip.Root>
+										</Table.Cell>
+										<Table.Cell>
+											{event?.namespace}
+										</Table.Cell>
+										<Table.Cell class="text-end">
+											<Tooltip.Root>
+												<Tooltip.Trigger class="cursor-help"
+													>{formatDistanceToNow(event.timestamp, {
+														addSuffix: true
+													})}</Tooltip.Trigger
+												>
+												<Tooltip.Content class="w-fit   ">
+													{format(event.timestamp, 'PPPppp')}</Tooltip.Content
+												>
+											</Tooltip.Root></Table.Cell
+										>
+									</Table.Row>
+								{:else}
+									<Table.Row class="fade-in">
+										<Table.Cell colspan={4} class="text-center text-sm text-muted-foreground">
+											No recent events.
+										</Table.Cell>
+									</Table.Row>
+								{/each}
+							</Table.Body>
+						</Table.Root>
+					</Card.Content>
+				</Card.Root>
+			</section>
+		</section>
+
+		<!-- <section class="grid grid-cols-2 gap-4">
 		<Card.Root>
 			<Card.Header>
 				<Card.Title>Agents</Card.Title>
@@ -195,18 +214,6 @@
 				</div>
 			</Card.Content>
 		</Card.Root>
-
-		<Card.Root>
-			<Card.Header>
-				<Card.Title>Messages (last hour)</Card.Title>
-			</Card.Header>
-			<Card.Content>
-				<div
-					class="bg-muted text-muted-foreground flex h-32 w-full items-center justify-center rounded-md text-xs"
-				>
-					Chart placeholder
-				</div>
-			</Card.Content>
-		</Card.Root>
-	</section>
+	</section> -->
+	{/if}
 </main>
