46d8eadaf400e9a49a0552c0e1ee7c731e0e3e92
Luka Forder <lukaforder@gmail.com>
2026-06-11 20:17:51 +0100

    feat: children for header

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -3,16 +3,21 @@
 	import { Separator } from '@coral-os/component-library/ui/separator/index.js';
 	import * as Sidebar from '@coral-os/component-library/ui/sidebar/index.js';
 	import Breadcrumbs from '$lib/components/Breadcrumbs.svelte';
+	import type { Snippet } from 'svelte';
 
-	let { override }: { override?: string } = $props();
+	let { override, children }: { override?: string; children?: Snippet } = $props();
 </script>
 
 <header class="bg-background sticky top-0 flex h-16 shrink-0 items-center gap-2 p-2">
 	<Card.Root class="bg-sidebar h-full w-full py-0">
 		<Card.Content class="relative flex h-full items-center gap-2 px-3">
 			<Sidebar.Trigger class="-ml-1" />
-			<Separator orientation="vertical" class="mr-2 h-4" />
-			<Breadcrumbs {override} />
+			<Separator orientation="vertical" class="{children ? '' : 'mr-2'}  h-4" />
+			{#if children}
+				{@render children()}
+			{:else}
+				<Breadcrumbs {override} />
+			{/if}
 		</Card.Content>
 	</Card.Root>
 </header>
