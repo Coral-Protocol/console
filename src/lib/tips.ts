573b604f9616ed698760e11125cdcafe87920c5b
Luka Forder <lukaforder@gmail.com>
2026-06-30 18:53:21 +0100

    wip: new tip

--- src/lib/tips.ts
+++ src/lib/tips.ts
@@ -1,8 +1,23 @@
 export const tips = [
 	{
-		tip: 'Hold shift when adding agents via the context menu to prevent the menu from closing'
+		tip: 'Hold Shift when adding agents via the context menu to prevent the menu from closing.'
 	},
 	{
 		tip: 'You can also drag and drop agents in to your request from the Market tab!'
+	},
+	{
+		tip: 'Scroll click empty space in the files tab to create a new tab, and scroll click a tab, to quickly close it.'
+	},
+	{
+		tip: 'Files are always saved! pick up where you left off at any time.'
+	},
+	{
+		tip: 'Untitled files with no content will be automatically deleted when you close Coral Console.'
+	},
+	{
+		tip: 'You can adjust the size of most panels in Console by selecting and dragging their borders.'
+	},
+	{
+		tip: 'Hold Shift while entering the Code view to enable debug mode and display internal data.'
 	}
 ];
