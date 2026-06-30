1b05037115944e4bbd86f7604911931d6171479a
Luka Forder <lukaforder@gmail.com>
2026-06-30 17:54:28 +0100

    wip: new tips file incoming...

---
+++ src/lib/tips.ts
@@ -0,0 +1,8 @@
+export const tips = [
+	{
+		tip: 'Hold shift when adding agents via the context menu to prevent the menu from closing'
+	},
+	{
+		tip: 'You can also drag and drop agents in to your request from the Market tab!'
+	}
+];
